9d651f3dc54f12f53afaeb8cd0c6e92eda2db99e
Nicky Marino <nsmarino96@gmail.com>
2019-11-22 22:58:02 -0600

    Update README and documentation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # node-text-api
+
+This is an example Node API using `express`, `body-parser`, and `to-zalgo`. More details can be found as a [blog post](https://nickymarino.com/2019/11/22/fast-intro-to-node-apis/)
+
+You can run this example by cloning the repo and running `npm start`
+
+## Endpoints
+
+- `/magic-8-ball`
+  - Returns `{"prediction": "<random_prediction>"}`, where `<random_prediction>` is randomly chosen from the standard Magic 8-Ball replies
+- `/to-zalgo`
+  - Input: `{"text": "your text here"}
+  - Returns: `{"code": 200, "original": "your text here", "zalgo": "zalgo-ified text"}`
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// Load the modules we installed
 const express = require('express')
 const bodyparser = require('body-parser')
 var toZalgo = require('to-zalgo')
 
+// Tell express to run the webserver on port 3200
 const app = express();
 const port = process.env.port || 3200
 
+// Use body-parser for unencoding API request bodies - more on this later
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
 
@@ -12,8 +15,11 @@ app.listen(port, () => {
     console.log(`running on port ${port}`)
 })
 
+// Return "Hello world" when you go to http://localhost:3200
 app.get('/', (req, res) => res.send('Hello world!'))
 
+// Return a random response for http://localhost:3200/magic-8-ball
+// Returns: {"prediction": "<random_prediction>"}
 app.get('/magic-8-ball', (req, res) => {
     const responses = [
         'It is certain.',
@@ -43,7 +49,11 @@ app.get('/magic-8-ball', (req, res) => {
     })
 })
 
+// Return Zalgo-ified text for http://localhost:3200/to-zalgo
+// Input:   {"text": "your text here"}
+// Returns: {"code": 200, "original": "your text here", "zalgo": "zalgo-ified text"}
 app.post('/to-zalgo', (req, res) => {
+    // Return 400 if the input doesn't contain a "text" element
     if (req.body.text === undefined) {
         res.status(400).json({
             "code": 400,
